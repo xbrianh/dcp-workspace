36df026571f95cfa5a5a57d3ca48ed1564de20e4
Brian Hannafious <bhannafi@ucsc.edu>
2019-03-07 07:34:45 -0800

    include gcp patters in git-secrets config

--- startup/setup_git.sh
+++ startup/setup_git.sh
@@ -8,6 +8,8 @@ git clone https://github.com/awslabs/git-secrets.git
 (cd git-secrets && sudo make install)
 git secrets --register-aws --global
 git secrets --install ~/.git-templates/git-secrets
+git secrets --add --global 'private_key'
+git secrets --add --global 'private_key_id'
 git config --global init.templateDir ~/.git-templates/git-secrets
 
 git config --global credential.helper store
